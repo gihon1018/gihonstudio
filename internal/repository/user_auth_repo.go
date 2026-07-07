2c65395ff31539950ab48691db31b33afede7d0b
gihon <gihon1018@qq.com>
2026-07-07 16:04:02 +0800

    实现 userAuthRepo 创建相关的一些功能

--- internal/repository/user_auth_repo.go
+++ internal/repository/user_auth_repo.go
@@ -18,3 +18,16 @@ func NewUserAuthRepository(db *gorm.DB) *UserAuthRepository {
 func (r *UserAuthRepository) Create(ctx context.Context, userAuth *UserAuth) error {
 	return gorm.G[UserAuth](r.db).Create(ctx, userAuth)
 }
+
+func (r *UserAuthRepository) CreateWithRows(ctx context.Context, userAuth *UserAuth) (int64, error) {
+	result := gorm.WithResult()
+	return result.RowsAffected, gorm.G[UserAuth](r.db, result).Create(ctx, userAuth)
+}
+
+func (r *UserAuthRepository) CreateOmit(ctx context.Context, userAuth *UserAuth, selectStr []string) error {
+	return gorm.G[UserAuth](r.db).Omit(selectStr...).Create(ctx, userAuth)
+}
+
+func (r *UserAuthRepository) CreateInBatches(ctx context.Context, userAuths *[]UserAuth, batchSize int) error {
+	return gorm.G[UserAuth](r.db).CreateInBatches(ctx, userAuths, batchSize)
+}
--- internal/repository/user_auth_repo_test.go
+++ internal/repository/user_auth_repo_test.go
@@ -47,10 +47,9 @@ func TestMain(m *testing.M) {
 	os.Exit(exitCode)
 }
 
-// 测试创建
 func TestUserAuthRepository_Create(t *testing.T) {
 	userAuth := &UserAuth{
-		Username:     "test_user_002",
+		Username:     "test_user_001",
 		PasswordHash: "123abc_hash",
 	}
 
@@ -59,4 +58,57 @@ func TestUserAuthRepository_Create(t *testing.T) {
 		t.Fatalf("创建失败：%v", err)
 	}
 	t.Logf("创建成功，UID：%d", userAuth.Uid)
+}
+
+func TestUserAuthRepository_CreateWithRows(t *testing.T) {
+	userAuth := &UserAuth{
+		Username:     "test_user_002",
+		PasswordHash: "123abc_hash",
+	}
+
+	rows, err := userAuthRepo.CreateWithRows(globalCtx, userAuth)
+	if err != nil {
+		t.Fatalf("创建失败：%v", err)
+	}
+	t.Logf("创建成功，UID：%d，影响行数：%d", userAuth.Uid, rows)
+}
+
+func TestUserAuthRepository_CreateOmit(t *testing.T) {
+	userAuth := &UserAuth{
+		Uid:          1002,
+		Username:     "test_user_003",
+		PasswordHash: "123abc_hash",
+		AccessToken:  "abc_123_token",
+	}
+
+	err := userAuthRepo.CreateOmit(globalCtx, userAuth, []string{"Uid", "AccessToken"})
+	if err != nil {
+		t.Fatalf("创建失败：%v", err)
+	}
+	t.Logf("创建成功，UID：%d", userAuth.Uid)
+}
+
+func TestUserAuthRepository_CreateInBatches(t *testing.T) {
+	userAuths := []UserAuth{
+		{
+			Username:     "test_user_004",
+			PasswordHash: "123abc_hash",
+		},
+		{
+			Username:     "test_user_005",
+			PasswordHash: "123abc_hash",
+		},
+		{
+			Username:     "test_user_006",
+			PasswordHash: "123abc_hash",
+		},
+	}
+
+	err := userAuthRepo.CreateInBatches(globalCtx, &userAuths, len(userAuths))
+	if err != nil {
+		t.Fatalf("创建失败：%v", err)
+	}
+	for _, userAuth := range userAuths {
+		t.Logf("创建成功，UID：%d", userAuth.Uid)
+	}
 }
